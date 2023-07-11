263a12d942f00acb34e900767be141ff2b2b13a5
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2023-07-11 15:32:38 +0200

    first version of form with one working textInput

--- src/BookingPage/BookingPage.tsx
+++ src/BookingPage/BookingPage.tsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import { TextInput } from "../TextInput/TextInput";
+
 export function BookingPage() {
-  return <>Booking</>;
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted!");
+    setName("");
+    console.log(name);
+  };
+  return (
+    <>
+      <h1>Booking</h1>
+
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <TextInput
+            id="name"
+            placeholder="name"
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {/* <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className=""
+            placeholder="name"
+            id="name"
+            value={name}
+          /> */}
+          <button type="submit" disabled={!name}>
+            Submit
+          </button>
+        </fieldset>
+      </form>
+    </>
+  );
 }
