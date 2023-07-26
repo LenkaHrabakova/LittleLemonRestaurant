76365288729296fd994003dab2096b11bc7481ba
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2023-07-26 17:52:01 +0200

    improvements in footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -19,18 +19,19 @@ export function Footer({
         <img src={logoSrc} alt={logoAlt} />
       </div>
       <div className="footer-info">
-        <ul className="footer-navigation">
-          Contact
+        <ul className="footer-address">
           <li>{address}</li>
           <li>{phone}</li>
           <li>{email}</li>
         </ul>
 
-        <ul className="footer-navigation">
-          Social media links
-          <li>Address</li>
-          <li>Phone number</li>
-          <li>Email</li>
+        <ul className="footer-social">
+          <li>
+            <img src="instagram.svg" alt="instagram" />
+          </li>
+          <li>
+            <img src="facebook.svg" alt="facebook" />
+          </li>
         </ul>
       </div>
     </footer>
