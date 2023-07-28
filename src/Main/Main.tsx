b6cf2e41270c236287991f68e2b903121a734ad9
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2023-07-28 17:59:46 +0200

    fix in Main component

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -3,5 +3,5 @@ import { ReactNode } from "react";
 import "./Main.css";
 
 export function Main({ children }: { children: ReactNode }) {
-  return <div className="main">{children}</div>;
+  return <main className="main">{children}</main>;
 }
