848ab95544296699631dfb00944c4f6fbf05fa20
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2023-07-29 14:27:37 +0200

    styling simplification in Nav and Header

--- src/Navigation/Nav.tsx
+++ src/Navigation/Nav.tsx
@@ -4,22 +4,22 @@ export function Nav() {
   return (
     <nav className="navigation">
       <ul className="navigation-list">
-        <li>
+        <li className="navigation-item">
           <a href="/#">Home</a>
         </li>
-        <li>
+        <li className="navigation-item">
           <a href="/about-us">About us</a>
         </li>
-        <li>
+        <li className="navigation-item">
           <a href="/menu">Menu</a>
         </li>
-        <li>
+        <li className="navigation-item">
           <a href="/reservation">Reservations</a>
         </li>
-        <li>
+        <li className="navigation-item">
           <a href="/online-order">Order online</a>
         </li>
-        <li>
+        <li className="navigation-item">
           <a href="/login">Login</a>
         </li>
       </ul>
