6d3058befe98cce32d320ee33db6dd76fe4984b2
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2023-07-30 19:45:03 +0200

    adding possibility to have icon in Button component

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -7,14 +7,19 @@ export function Button({
   disabled,
   type,
   className,
+  imgSrc,
+  imgAlt,
 }: {
-  title: string;
+  title?: string;
   disabled?: boolean;
   type: ButtonType;
   className?: string;
+  imgSrc?: string;
+  imgAlt?: string;
 }) {
   return (
     <button disabled={disabled} type={type} className={className}>
+      {imgSrc && <img src={imgSrc} alt={imgAlt} className="button-icon" />}
       {title}
     </button>
   );
