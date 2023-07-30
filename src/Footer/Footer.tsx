2d871bf65bcfb0d115b458787ef3db8d6aa51cda
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2023-07-30 16:22:55 +0200

    styling updates in footer - links

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -21,16 +21,23 @@ export function Footer({
       <div className="footer-info">
         <ul className="footer-address">
           <li>{address}</li>
-          <li>{phone}</li>
-          <li>{email}</li>
-        </ul>
-
-        <ul className="footer-social">
           <li>
-            <img src="instagram.svg" alt="instagram" />
+            <a href="tel:{phone}">{phone}</a>
           </li>
           <li>
-            <img src="facebook.svg" alt="facebook" />
+            <a href="mailto:{email}">{email}</a>
+          </li>
+        </ul>
+        <ul className="footer-social">
+          <li>
+            <a href="https://www.instagram.com/">
+              <img src="instagram.svg" alt="instagram" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/">
+              <img src="facebook.svg" alt="facebook" />
+            </a>
           </li>
         </ul>
       </div>
